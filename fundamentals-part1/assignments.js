79cee93d3b94976007d83acad868b577a619d636
DomWat <domwat20@gmail.com>
2021-01-14 18:04:25 -0600

    section 2

---
+++ fundamentals-part1/assignments.js
@@ -0,0 +1,9 @@
+// assigment 1
+
+let country = 'US'
+let continent = 'North America'
+let countryPopulation = '330 million'
+
+console.log(country)
+console.log(continent)
+console.log(countryPopulation)
